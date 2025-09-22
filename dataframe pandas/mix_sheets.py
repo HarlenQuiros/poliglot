f74c4a92e034076557897fa92a100475ed2b4e06
EsRoHi26 <es.rojas@estudiantec.cr>
2025-09-22 07:57:13 -0600

    Script SOLO y Manual

--- dataframe pandas/mix_sheets.py
+++ dataframe pandas/mix_sheets.py
@@ -1,24 +1,40 @@
 import os
 import pandas as pd
 
-df1 = pd.read_excel("Lista_ejercicio_nombre_carnet.xlsx", sheet_name="Sheet1")
+df1 = pd.read_csv("Mapeo_estudiantes_anonimizado.csv", encoding="cp1252", sep=";")
+df2 = pd.read_excel("Clasificación_y_métricas de soluciones.xlsx", sheet_name="Compress 2", header=1)
 
-df2 = pd.read_excel("Evaluacion_rondas_consensuado.xlsx", sheet_name="Evaluacion ABS", header=None)
-df2.columns = ["Ex", "Evaluador #1", "Comentario #1", "Evaluador #2", "Comentario #2", "Evaluador #3", "Comentario #3", "Trash"]
+merged_df = pd.merge(df1, df2, left_on="# de carnet", right_on="Carnet", how="right")
+merged_df = merged_df[(merged_df["Tipo de ejercicio"] == 2)]
+merged_df["Nivel_Abstracción_MANUAL profes"] = merged_df["Evaluación Manual"]
+merged_df = merged_df.drop(columns=["Evaluación Manual"])
 
-df3 = pd.read_excel("Clasificación_y_métricas de soluciones.xlsx", sheet_name="Compress 2")
+merged_df.to_excel("2.xlsx", index=False, sheet_name="Compress 2")
 
-df1["key"] = df1["Archivo"].str.extract(r'(\d+)').astype(int)
-df2["key"] = df2["Ex"].str.extract(r'(\d+)').fillna(0).astype(int)
+df2 = pd.read_excel("Clasificación_y_métricas de soluciones.xlsx", sheet_name="Zoologico 1", header=1)
+merged_df = pd.merge(df1, df2, left_on="# de carnet", right_on="Carnet", how="right")
+merged_df = merged_df[(merged_df["Tipo de ejercicio"] == 1)]
+merged_df["Nivel_Abstracción_MANUAL profes"] = merged_df["Evaluación Manual"]
+merged_df = merged_df.drop(columns=["Evaluación Manual"])
 
-merged_df = pd.merge(df1, df2, on="key", how="inner")
-merged_df = merged_df.drop(columns=["Ex","Comentario #1", "Evaluador #2", "Comentario #2", "Evaluador #3", "Comentario #3", "Trash", "Nombre", "Archivo", "Ejercicio", "Grupo", "Corre", "Resuelve", "Métricas"])
+merged_df.to_excel("1.xlsx", index=False, sheet_name="Zoologico 1")
 
-df3["Key"] = df3.iloc[:, 0]
+df2 = pd.read_excel("Clasificación_y_métricas de soluciones.xlsx", sheet_name="Conteo 3", header=1)
+merged_df = pd.merge(df1, df2, left_on="# de carnet", right_on="Carnet", how="right")
+merged_df = merged_df[(merged_df["Tipo de ejercicio"] == 3)]
+merged_df["Nivel_Abstracción_MANUAL profes"] = merged_df["Evaluación Manual"]
+merged_df = merged_df.drop(columns=["Evaluación Manual"])
+merged_df["Evaluación SOLO"] =merged_df["SOLO"]
+merged_df = merged_df.drop(columns=["SOLO"])
 
-merged_df = df3.merge(merged_df[["Carnet", "Evaluador #1"]], left_on="Key", right_on="Carnet", how="left")
-merged_df.iloc[:, 1] = merged_df["Evaluador #1"]
+merged_df.to_excel("3.xlsx", index=False, sheet_name="Conteo 3")
 
-merged_df = merged_df.drop(columns=["Carnet", "Evaluador #1", "Key"])
+df2 = pd.read_excel("Clasificación_y_métricas de soluciones.xlsx", sheet_name="Shiritori 4", header=1)
+merged_df = pd.merge(df1, df2, left_on="# de carnet", right_on="Carnet", how="right")
+merged_df = merged_df[(merged_df["Tipo de ejercicio"] == 4)]
+merged_df["Nivel_Abstracción_MANUAL profes"] = merged_df["Evaluación Manual"]
+merged_df = merged_df.drop(columns=["Evaluación Manual"])
+merged_df["Evaluación SOLO"] =merged_df["SOLO"]
+merged_df = merged_df.drop(columns=["SOLO"])
 
-merged_df.to_excel("temporal.xlsx", index=False)
+merged_df.to_excel("4.xlsx", index=False, sheet_name="Shiritori 4")
